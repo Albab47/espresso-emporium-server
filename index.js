2a56291869091975b3d5ccdcc8cf3cfd772cff90
Albab47 <115328782+Albab47@users.noreply.github.com>
2024-04-25 07:20:37 +0600

    Setup express, connnect mongoDB and add get, post route

---
+++ index.js
@@ -0,0 +1,77 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express()
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+// connection string
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.173efa4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a new client and connect to MongoDB
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+ 
+    // Connect to the "espressoDB" database and access its "products" collection
+    const coffeeCollection = client.db('espressoDB').collection('coffees')
+
+    // Add new coffee
+    app.post('/coffees', async(req, res) => {
+      const newCoffee = req.body;
+      const result = await coffeeCollection.insertOne(newCoffee)
+      res.send(result);
+    })
+
+    // Get all coffees
+    app.get('/coffees', async(req, res) => {
+        const cursor = coffeeCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
+    // Get single coffee
+    app.get('/coffees/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const coffee = await coffeeCollection.findOne(query)
+      res.send(coffee);
+    })
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send("Espresso emporium Server's is running")
+})
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+})
+
