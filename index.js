5b1e45c81af90c1ad7bbbd09a9d3d5ee7085ff03
Albab47 <115328782+Albab47@users.noreply.github.com>
2024-04-25 08:38:08 +0600

    Add route to res put request

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
-const app = express()
+const app = express();
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // connection string
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.173efa4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -18,46 +18,68 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
- 
+
     // Connect to the "espressoDB" database and access its "products" collection
-    const coffeeCollection = client.db('espressoDB').collection('coffees')
+    const coffeeCollection = client.db("espressoDB").collection("coffees");
 
     // Add new coffee
-    app.post('/coffees', async(req, res) => {
+    app.post("/coffees", async (req, res) => {
       const newCoffee = req.body;
-      const result = await coffeeCollection.insertOne(newCoffee)
+      const result = await coffeeCollection.insertOne(newCoffee);
       res.send(result);
-    })
+    });
 
     // Get all coffees
-    app.get('/coffees', async(req, res) => {
-        const cursor = coffeeCollection.find()
-        const result = await cursor.toArray()
-        res.send(result)
-    })
+    app.get("/coffees", async (req, res) => {
+      const cursor = coffeeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     // Get single coffee
-    app.get('/coffees/:id', async(req, res) => {
+    app.get("/coffees/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const coffee = await coffeeCollection.findOne(query)
+      const query = { _id: new ObjectId(id) };
+      const coffee = await coffeeCollection.findOne(query);
       res.send(coffee);
-    })
+    });
 
+    // Update coffee details
+    app.put("/coffees/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedCoffee = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = {upsert: true}
+      const coffee = {
+        $set: {
+          name: updatedCoffee.name,
+          chef: updatedCoffee.chef,
+          supplier: updatedCoffee.supplier,
+          taste: updatedCoffee.taste,
+          category: updatedCoffee.category,
+          details: updatedCoffee.details,
+          photo: updatedCoffee.photo,
+        },
+      };
+      const result = await coffeeCollection.updateOne(filter, coffee, options);
+      res.send(result);
+    });
 
 
 
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -65,13 +87,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send("Espresso emporium Server's is running")
-})
-
+app.get("/", (req, res) => {
+  res.send("Espresso emporium Server's is running");
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-})
-
+  console.log(`Server is running on port: ${port}`);
+});
